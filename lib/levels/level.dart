02b0fe3d3f12e6709f94af639dbbf60b7007347e
ykcho5 <yoonkyoung.cho@visitandregular.com>
2024-09-27 16:40:00 +0900

    feat: add player

--- lib/levels/level.dart
+++ lib/levels/level.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:pixel_adventure/actors/player.dart';
 
 class Level extends World {
   late TiledComponent level;
@@ -11,6 +12,18 @@ class Level extends World {
     level = await TiledComponent.load('Level-01.tmx', Vector2.all(16));
 
     add(level);
+
+    final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
+    for(final spawnPoint in spawnPointsLayer!.objects){
+      switch (spawnPoint.class_) {
+        case 'Player':
+          // final player = Player
+          break;
+        default:
+      }
+    }
+
+    add(Player(character: 'Mask Dude'));
 
     return super.onLoad();
   }
