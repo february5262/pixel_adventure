2020d374c82538ed76fcff7e4da929f351a9c177
february5262 <february5262@gmail.com>
2024-10-03 23:26:52 +0900

    feat: add player movement control (keyboard, joystick)

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flutter/services.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 enum PlayerState { idle, running }
@@ -8,9 +9,9 @@ enum PlayerState { idle, running }
 enum PlayerDirection { left, right, none }
 
 class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<PixelAdventure> {
-  Player({position, required this.character}) : super(position: position);
+    with HasGameRef<PixelAdventure>, KeyboardHandler {
   String character;
+  Player({position, this.character = 'Ninja Frog'}) : super(position: position);
 
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
@@ -19,6 +20,7 @@ class Player extends SpriteAnimationGroupComponent
   PlayerDirection playerDirection = PlayerDirection.none;
   double moveSpeed = 100;
   Vector2 velocity = Vector2.zero();
+  bool isFacingRight = true;
 
   @override
   FutureOr<void> onLoad() {
@@ -30,6 +32,25 @@ class Player extends SpriteAnimationGroupComponent
   void update(double dt) {
     _updatePlayerMovement(dt);
     super.update(dt);
+  }
+
+  @override
+  bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    final isLeftKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyA) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final isRightKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyD) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowRight);
+
+    if (isLeftKeyPressed && isRightKeyPressed) {
+      playerDirection = PlayerDirection.none;
+    } else if (isLeftKeyPressed) {
+      playerDirection = PlayerDirection.left;
+    } else if (isRightKeyPressed) {
+      playerDirection = PlayerDirection.right;
+    } else {
+      playerDirection = PlayerDirection.none;
+    }
+    return super.onKeyEvent(event, keysPressed);
   }
 
   void _loadAllAnimations() {
@@ -61,13 +82,28 @@ class Player extends SpriteAnimationGroupComponent
     double dirX = 0.0;
     switch (playerDirection) {
       case PlayerDirection.left:
-      dirX -= moveSpeed;
+        if (isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = false;
+        }
+        current = PlayerState.running;
+        dirX -= moveSpeed;
         break;
       case PlayerDirection.right:
+        if (!isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = true;
+        }
+        current = PlayerState.running;
+        dirX += moveSpeed;
         break;
       case PlayerDirection.none:
+        current = PlayerState.idle;
         break;
       default:
     }
+
+    velocity = Vector2(dirX, 0.0);
+    position += velocity * dt;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Flame.device.fullScreen();
-  Flame.device.setLandscape();
-  
+  // async, await 없으면 real device에서 portrait 상태에서 있다가 landscape으로 바뀌기 때문에 설정해줘야 함
+  // 에뮬에서는 문제가 없음 ㅋㅋ
+  await Flame.device.fullScreen();
+  await Flame.device.setLandscape();
+
   PixelAdventure game = PixelAdventure();
   runApp(GameWidget(game: kDebugMode ? PixelAdventure() : game));
 }
