29abf131135a51ada2e248c4140641448d4a91d9
february5262 <february5262@gmail.com>
2024-10-12 17:08:19 +0900

    feat: saw collision

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -6,10 +6,11 @@ import 'package:flutter/services.dart';
 import 'package:pixel_adventure/components/collision_block.dart';
 import 'package:pixel_adventure/components/custom_hitbox.dart';
 import 'package:pixel_adventure/components/fruit.dart';
+import 'package:pixel_adventure/components/saw.dart';
 import 'package:pixel_adventure/components/utils.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
-enum PlayerState { idle, running, jumping, falling }
+enum PlayerState { idle, running, jumping, falling, hit, appearing }
 
 class Player extends SpriteAnimationGroupComponent
     with HasGameRef<PixelAdventure>, KeyboardHandler, CollisionCallbacks {
@@ -21,6 +22,8 @@ class Player extends SpriteAnimationGroupComponent
   late final SpriteAnimation runningAnimation;
   late final SpriteAnimation jumpingAnimation;
   late final SpriteAnimation fallingAnimation;
+  late final SpriteAnimation hitAnimation;
+  late final SpriteAnimation appearingAnimation;
 
   final double _gravity = 9.8;
   final double _jumpForce = 260;
@@ -28,9 +31,13 @@ class Player extends SpriteAnimationGroupComponent
 
   double horizontalMovement = 0;
   double moveSpeed = 100;
+
+  Vector2 startingPosition = Vector2.zero();
+
   Vector2 velocity = Vector2.zero();
   bool isOnGround = false;
   bool hasJumped = false;
+  bool gotHit = false;
   List<CollisionBlock> collisionBlocks = [];
   CustomHitbox hitbox =
       CustomHitbox(offsetX: 10, offsetY: 4, width: 14, height: 28);
@@ -39,6 +46,9 @@ class Player extends SpriteAnimationGroupComponent
   FutureOr<void> onLoad() {
     _loadAllAnimations();
     // debugMode = true;
+
+    startingPosition = Vector2(position.x, position.y);
+
     add(RectangleHitbox(
         position: Vector2(hitbox.offsetX, hitbox.offsetY),
         size: Vector2(hitbox.width, hitbox.height)));
@@ -47,12 +57,14 @@ class Player extends SpriteAnimationGroupComponent
 
   @override
   void update(double dt) {
-    _updatePlayerState();
-    _updatePlayerMovement(dt);
-    _checkHorizontalCollisions();
-    // do gravity after horizontal Collision
-    _applyGravity(dt);
-    _checkVerticalCollisions();
+    if (!gotHit) {
+      _updatePlayerState();
+      _updatePlayerMovement(dt);
+      _checkHorizontalCollisions();
+      // do gravity after horizontal Collision
+      _applyGravity(dt);
+      _checkVerticalCollisions();
+    }
     super.update(dt);
   }
 
@@ -74,6 +86,7 @@ class Player extends SpriteAnimationGroupComponent
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
     if (other is Fruit) other.collidedWithPlayer();
+    if (other is Saw) _respawn();
     super.onCollision(intersectionPoints, other);
   }
 
@@ -82,13 +95,17 @@ class Player extends SpriteAnimationGroupComponent
     runningAnimation = _spriteAnimation('Run', 12);
     jumpingAnimation = _spriteAnimation('Jump', 1);
     fallingAnimation = _spriteAnimation('Fall', 1);
+    hitAnimation = _spriteAnimation('Hit', 7);
+    appearingAnimation = _specialSpriteAnimation('Appearing', 7);
 
     // List of all animations
     animations = {
       PlayerState.idle: idleAnimation,
       PlayerState.running: runningAnimation,
       PlayerState.jumping: jumpingAnimation,
-      PlayerState.falling: fallingAnimation
+      PlayerState.falling: fallingAnimation,
+      PlayerState.hit: hitAnimation,
+      PlayerState.appearing: appearingAnimation,
     };
 
     // Set current animation
@@ -102,6 +119,17 @@ class Player extends SpriteAnimationGroupComponent
         amount: amount,
         stepTime: stepTime,
         textureSize: Vector2.all(32),
+      ),
+    );
+  }
+
+  SpriteAnimation _specialSpriteAnimation(String state, int amount) {
+    return SpriteAnimation.fromFrameData(
+      game.images.fromCache('Main Characters/$state (96x96).png'),
+      SpriteAnimationData.sequenced(
+        amount: amount,
+        stepTime: stepTime,
+        textureSize: Vector2.all(96),
       ),
     );
   }
@@ -199,5 +227,25 @@ class Player extends SpriteAnimationGroupComponent
         }
       }
     }
+  }
+
+  void _respawn() {
+    const hitDuration = Duration(milliseconds: 350); // 50 millisec * 7 frame
+    const appearingDuration = Duration(milliseconds: 350);
+    const canMoveDuration = Duration(milliseconds: 400);
+    gotHit = true;
+    current = PlayerState.hit;
+    Future.delayed(hitDuration, () {
+      scale.x = 1; // modify facing state of spawn character
+      position = startingPosition - Vector2.all(32);
+      current = PlayerState.appearing;
+      Future.delayed(appearingDuration, () {
+        velocity = Vector2.zero();
+        position = startingPosition;
+        _updatePlayerState();
+        gotHit = false;
+        // Future.delayed(canMoveDuration, () => gotHit = false);
+      });
+    });
   }
 }
--- lib/components/saw.dart
+++ lib/components/saw.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
@@ -26,15 +27,47 @@ class Saw extends SpriteAnimationComponent with HasGameRef<PixelAdventure> {
   @override
   FutureOr<void> onLoad() {
     priority = -1;
+    add(CircleHitbox());
 
     if (isVertical) {
       rangeNeg = position.y - offNeg * tilesize;
-      rangeNeg = position.y + offNeg * tilesize;
+      rangePos = position.y + offPos * tilesize;
+    } else {
+      rangeNeg = position.x - offNeg * tilesize;
+      rangePos = position.x + offPos * tilesize;
     }
     animation = SpriteAnimation.fromFrameData(
         game.images.fromCache('Traps/Saw/On (38x38).png'),
         SpriteAnimationData.sequenced(
             amount: 8, stepTime: sawSpeed, textureSize: Vector2.all(38)));
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if (isVertical) {
+      _moveVertically(dt);
+    } else {
+      _moveHorizontally(dt);
+    }
+    super.update(dt);
+  }
+
+  void _moveVertically(double dt) {
+    if (position.y >= rangePos) {
+      moveDirection = -1;
+    } else if (position.y <= rangeNeg) {
+      moveDirection = 1;
+    }
+    position.y += moveDirection * moveSpeed * dt;
+  }
+
+  void _moveHorizontally(double dt) {
+    if (position.x >= rangePos) {
+      moveDirection = -1;
+    } else if (position.x <= rangeNeg) {
+      moveDirection = 1;
+    }
+    position.x += moveDirection * moveSpeed * dt;
   }
 }
